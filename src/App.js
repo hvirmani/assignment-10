adee15802f6cc998a8116943023f1c9aebdc24d1
hvirmani <harshit0030.cse19@chitkara.edu.in>
2021-12-04 05:02:19 +0530

    Assignment-10

---
+++ src/App.js
@@ -0,0 +1,161 @@
+export default function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul id="navbar">
+          <li>
+            <a href="#section1">Being geeky</a>
+          </li>
+          <li>
+            <a href="#section2">tutorials</a>
+          </li>
+          <li>
+            <a href="#section3">Video</a>
+          </li>
+          <li>
+            <a href="#section5">Coupon</a>
+          </li>
+        </ul>
+      </nav>
+      <section id="section1">
+        <h1>Being Geeky</h1>
+        <h3>Your own video and theortical study place.</h3>
+        <p>
+          Being Geeky is one of the best platform for the online learning. With
+          the best study material.Dummy text is text that is used in the
+          publishing industry or by web designers to occupy the space which will
+          later be filled with 'real' content. This is required when, for
+          example, the final text is not yet available. Dummy text is also known
+          as 'fill text'. It is said that song composers of the past used dummy
+          texts as lyrics when writing melodies in order to have a 'ready-made'
+          text to sing with the melody. Dummy texts have been in use by
+          typesetters since the 16th century.
+        </p>
+      </section>
+      <section id="section2">
+        <h2>
+          <i></i>Documentation
+        </h2>
+        <h3>All genuine Documentation at one place</h3>
+        <p>
+          Being Geeky helps the viewer to find all the required documentation at
+          one place so that they need not waste time to find offical
+          documentation. The most well-known dummy text is the 'Lorem Ipsum',
+          which is said to have originated in the 16th century. Lorem Ipsum is
+          composed in a pseudo-Latin language which more or less corresponds to
+          'proper' Latin. It contains a series of real Latin words. This ancient
+          dummy text is also incomprehensible, but it imitates the rhythm of
+          most European languages in Latin script. The advantage of its Latin
+          origin and the relative meaninglessness of Lorum Ipsum is that the
+          text does not attract attention to itself or distract the viewer's
+          attention from the layout.
+        </p>
+        <a href="#">Start your trial now</a>
+        <small>You can download Being Geeky from app store or playstore.</small>
+        <img
+          alt="dont know"
+          src="https://beinggeeky-hnip75cdkgvvk6vt.web.codequotient.com/public/ipad-app-mockup-03.png"
+        />
+      </section>
+      <section id="section3">
+        <h3>The video lecture you shouldn't miss</h3>
+        <p>
+          With one of the best content available online.And one of the best
+          instructors it makes Being Geeky one of the best available learing
+          platform available.he spread of computers and layout programmes thus
+          made dummy text better known. While in earlier times several lines of
+          the Lorem Ipsum text were repeated in the creation of dummy texts,
+          today the full text of Cicero's 'De finibus' serves as the basis for
+          many dummy text or Lorem Ipsum generators. Based on 'De finibus',
+          these generators automatically create longer sections of the Lorem
+          Ipsum text or various other filler texts.
+        </p>
+        <a href="#">Read More</a>
+        <img
+          alt="dont know"
+          src="https://beinggeeky-hnip75cdkgvvk6vt.web.codequotient.com/public/lecture.jpg"
+        />
+      </section>
+      <section id="section4">
+        <h3>Lectures available anywhere anytime.</h3>
+        <p>
+          You can watch the lecture anytime from anywhere and ask doubts.The
+          phrasal sequence of the Lorem Ipsum text is now so widespread and
+          commonplace that many DTP programmes can generate dummy text using the
+          starting sequence "Lorem ipsum". Fortunately, the phrase 'Lorem Ipsum'
+          is now recognized by electronic pre-press systems and, when found, an
+          alarm can be raised. This avoids a publication going to print with
+          overlooked dummy text.
+        </p>
+      </section>
+      <section id="section5">
+        <div id="img">
+          <img
+            alt="dont know"
+            src="https://beinggeeky-hnip75cdkgvvk6vt.web.codequotient.com/public/coupon.jpg"
+          />
+        </div>
+        <div id="info">
+          <h5>Coupons</h5>
+          <p>
+            You can apply different coupons to get extra discount on selected
+            courses.Certain internet providers exploit the fact that fill text
+            cannot be recognized by automatic search engines - meaningful
+            information cannot be distinguished from meaningless:
+            Target-generated dummy text mixed with a certain combination of
+            search terms can lead to an increased frequency of visits by search
+            machine users. As a consequence, advertising revenues, which rely on
+            website 'hits', are increased.
+          </p>
+          <p>Already have Coupon?</p>
+          <hr />
+          <a href="#">
+            <i></i>Redeem
+          </a>
+        </div>
+      </section>
+      <footer>
+        <div>
+          <h6>Address</h6>
+          <ul className="details">
+            <li>ZZZZ Workspace</li>
+            <li>Street 43,building 4A</li>
+            <li>Industrial Area</li>
+            <li>Aqwertyu</li>
+            <li>Mumbai</li>
+          </ul>
+        </div>
+        <div>
+          <h6>Clients</h6>
+          <ul className="details">
+            <li>XYZ org</li>
+            <li>ABC org</li>
+            <li>IJK org</li>
+            <li>Qwerty Corp</li>
+            <li>ZAQ PVT</li>
+          </ul>
+        </div>
+        <div>
+          <h6>Stack</h6>
+          <ul className="details">
+            <li>HTML5,CSS,JS</li>
+            <li>React</li>
+            <li>Angular</li>
+            <li>NodeJS</li>
+            <li>Django</li>
+          </ul>
+        </div>
+        <div>
+          <h6>About Being Geeky</h6>
+          <ul className="details">
+            <li>About CEO</li>
+            <li>About Team</li>
+            <li>Location</li>
+            <li>Working Atmosphere</li>
+            <li>Contact Being Geeky</li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  );
+}
